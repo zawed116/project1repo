3b51b800f1b8c23f9ca9863d9f21af0b05e3096a
Zawed <zawedakhter5786@gmail.com>
2025-07-10 23:47:39 +0530

    hello

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/project1repo/', // 👈 important! Replace with your GitHub repo name exactly
   plugins: [react()],
 })
