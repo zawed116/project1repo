758aa2a15a0f35f08ed77a25abc822cfa662b79e
Zawed <zawedakhter5786@gmail.com>
2025-07-10 23:12:24 +0530

    jj

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base:'/zawed116/newproject/',
+  base: '/project1repo/',  // <-- match your GitHub repo name if using gh-pages
   plugins: [react()],
 })
